af0897a6d0855b0028a6500130142b4244480d09
klikkolee <klikkolee@gmail.com>
2016-06-20 15:01:44 -0700

    fixed typo in SDL compatibility fix which prevented building on visual studio 2015

--- Engine/SDL_fix.cpp
+++ Engine/SDL_fix.cpp
@@ -3,7 +3,7 @@ SDL2 depends on __iob_func, which is replaced by __acrt_iob_func in vs 2015
 the two are differing types and dynamically linked,
 thus #define __iob_func __acrt_iob_func will not work
 */
-#if _MSC_Ver >= 1900 
+#if _MSC_VER >= 1900 
 #include <stdio.h>
 FILE _iob[] = { *stdin, *stdout, *stderr };
 extern "C" FILE * __iob_func(void)
