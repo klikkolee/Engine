d40760064d557940ad98d31079153fa8e67d57ba
klikkolee <klikkolee@gmail.com>
2016-06-20 14:09:34 -0700

    fixed incompatibility between Visual Studio 2015 and SDL2 changed project settings to work correctly with SDL2 and prevent console from appearing

---
+++ Engine/SDL_fix.cpp
@@ -0,0 +1,13 @@
+/*
+SDL2 depends on __iob_func, which is replaced by __acrt_iob_func in vs 2015
+the two are differing types and dynamically linked,
+thus #define __iob_func __acrt_iob_func will not work
+*/
+#if _MSC_Ver >= 1900 
+#include <stdio.h>
+FILE _iob[] = { *stdin, *stdout, *stderr };
+extern "C" FILE * __iob_func(void)
+{
+	return _iob;
+}
+#endif
